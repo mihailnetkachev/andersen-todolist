3ca6ba002ff29172c7c1272912da593cdccb5730
mihailnetkachev <mihailnetkachev.8@gmail.com>
2018-06-05 16:24:26 +0300

    New functionality was added.

--- src/js/index.js
+++ src/js/index.js
@@ -6,19 +6,86 @@ function setStorage(tasks) {
   localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
-function renderStorage() {
-  const tasks = getStorage();
+function renderStorage(tasksArr) {
+  const tasks = tasksArr || getStorage();
   let counter = '0';
   list.innerHTML = '';
 
-  tasks.forEach((item) => {
-    list.appendChild(createHTMLConstruction(item));
-    if (!item.isComplete) {
-      counter++;
+  if (tasks.length === 0) {
+    list.innerHTML = 'You don\'t have any tasks';
+    return;
+  }
+
+  tasks.forEach((item, index) => {
+
+    if (index >= firstTaskToRender && index < firstTaskToRender + numberOfTaskToRender) {
+
+      let isAdded = false;
+
+      for (let i = 0; i < list.length; i++) {
+        const task = list[i];
+
+        if (item.id === +task.getAttribute('id')) {
+          if (item.edited !== +task.getAttribute('data-edited')
+            || item.text !== task.getElementsByClassName('taskfield__itemText')[0].textContent) {
+            list.replaceChild(createHTMLConstruction(item), task);
+          } else {
+            isAdded = true;
+          }
+        }
+      }
+
+      if (!isAdded) {
+        list.appendChild(createHTMLConstruction(item));
+        if (!item.isComplete) {
+          counter++;
+        }
+      }
+
     }
+
+
+
   });
 
   headerCounter.textContent = counter;
+}
+
+function renderStorageWithSort(typeOfSort) {
+  const tasks = getStorage();
+
+  tasks.sort((a, b) => {
+    return (a[typeOfSort] > b[typeOfSort]) ? 1 : -1;
+  });
+
+  renderStorage(tasks);
+}
+
+function createPageList() {
+  const numberOfTasks = getStorage().length;
+  let numberOfPages = Math.ceil(numberOfTasks / numberOfTaskToRender);
+  let numberOfPage = 1;
+  let indexOfPage = 0;
+
+  pageList.innerHTML = '';
+  for (let i = 0; i < numberOfPages; i++) {
+    pageList.appendChild(createPageIcon({number: numberOfPage++, index: indexOfPage}));
+    indexOfPage += numberOfTaskToRender;
+  }
+}
+
+function createPageIcon(options) {
+  const pageItem = document.createElement('li');
+
+  pageItem.classList.add('taskfield__page');
+  pageItem.textContent = options.number;
+
+  pageItem.onclick = () => {
+    firstTaskToRender = options.index;
+    renderStorage();
+  };
+
+  return pageItem;
 }
 
 class Task {
@@ -26,6 +93,7 @@ class Task {
     this.id = options.id;
     this.text = options.text;
     this.isComplete = false;
+    this.edited = this.id;
   }
 }
 
@@ -39,6 +107,7 @@ function addTask(item) {
 
   tasks.push(item);
   setStorage(tasks);
+  createPageList();
 }
 
 function removeTask() {
@@ -53,8 +122,18 @@ function removeTask() {
 
   setStorage(tasks);
   renderStorage();
+  createPageList();
 
   event.preventDefault();
+}
+
+function editTask() {
+  const tasks = getStorage();
+  const task = this.parentNode;
+  let taskText = task.getElementsByClassName('taskfield__itemText')[0].textContent;
+  const tooltip = createTooltip(taskText);
+
+  task.appendChild(tooltip);
 }
 
 function triggeringComplete() {
@@ -72,14 +151,17 @@ function triggeringComplete() {
 }
 
 function createHTMLConstruction(options) {
-  let listItem = document.createElement('li');
-  let label = document.createElement('label');
-  let input = document.createElement('input');
-  let span = document.createElement('span');
-  let a = document.createElement('a');
+  const listItem = document.createElement('li');
+  const label = document.createElement('label');
+  const input = document.createElement('input');
+  const span = document.createElement('span');
+  const createDate = document.createElement('span');
+  const editButton = document.createElement('a');
+  const a = document.createElement('a');
 
   listItem.classList.add('taskfield__item');
   listItem.setAttribute('id', options.id);
+  listItem.setAttribute('data-edited', options.edited);
   label.classList.add('taskfield__itemLabel');
   input.classList.add('taskfield__itemCheckbox');
   input.setAttribute('type', 'checkbox');
@@ -88,24 +170,74 @@ function createHTMLConstruction(options) {
   }
   span.classList.add('taskfield__itemText');
   span.textContent = options.text;
+  createDate.classList.add('taskfield__itemDate');
+  createDate.textContent = ''
+    + (new Date(options.id)).getDate() + '.' + (+(new Date(options.id).getMonth()) + 1) + '.' + (new Date(options.id).getFullYear());
   a.classList.add('taskfield__itemDelete');
   a.textContent = '+';
+  editButton.classList.add('taskfield__itemEdit');
+  editButton.textContent = 'Edit';
 
   input.onclick = triggeringComplete;
   a.onclick = removeTask;
+  editButton.onclick = editTask;
 
   label.appendChild(input);
   label.appendChild(span);
   listItem.appendChild(label);
+  listItem.appendChild(createDate);
+  listItem.appendChild(editButton);
   listItem.appendChild(a);
 
   return listItem;
 }
 
-let input = document.getElementById('inputfield__input');
-let button = document.getElementById('inputfield__button');
-let list = document.getElementById('taskfield__list');
-let headerCounter = document.getElementById('taskfield__headerValue');
+function createTooltip(value) {
+  const wrapper = document.createElement('div');
+  const inputField = document.createElement('input');
+  const inputButton = document.createElement('input');
+
+  wrapper.classList.add('tooltip');
+  inputField.classList.add('tooltip__inputField');
+  inputField.setAttribute('type', 'text');
+  inputField.setAttribute('placeholder', 'I changed my ming ...');
+  inputField.setAttribute('value', value || '');
+  inputButton.classList.add('tooltip__inputButton');
+  inputButton.setAttribute('type', 'submit');
+  inputButton.value = 'Change';
+
+  inputButton.onclick = () => {
+    let task = inputButton.parentNode.parentNode;
+    let tasks = getStorage();
+
+    tasks.forEach((item) => {
+      if (item.id === +task.getAttribute('id')) {
+        item.text = inputField.value;
+        item.edited = +new Date();
+      }
+    });
+
+
+    setStorage(tasks);
+    renderStorage();
+  };
+
+  wrapper.appendChild(inputField);
+  wrapper.appendChild(inputButton);
+  return wrapper;
+}
+
+const input = document.getElementById('inputfield__input');
+const button = document.getElementById('inputfield__button');
+const list = document.getElementById('taskfield__list');
+const headerCounter = document.getElementById('taskfield__headerValue');
+const sortbttnByDate = document.getElementById('button__byDate');
+const sortbttnAlphabetically = document.getElementById('button__alphabetically');
+const sortbttnByEditingDate = document.getElementById('button__byEditingDate');
+const searchInput = document.getElementById('searchfield__input');
+let firstTaskToRender = 0;
+const numberOfTaskToRender = 3;
+const pageList = document.getElementById('taskfield__pageList');
 
 button.addEventListener('click', function (event) {
   event.preventDefault();
@@ -124,5 +256,37 @@ button.addEventListener('click', function (event) {
 
   renderStorage();
 });
+
+sortbttnByDate.onclick = () => {
+  renderStorageWithSort('id');
+};
+
+sortbttnAlphabetically.onclick = () => {
+  renderStorageWithSort('text');
+};
+
+sortbttnByEditingDate.onclick = () => {
+  renderStorageWithSort('edited');
+};
+
+searchInput.onkeyup = () => {
+  let tasks = getStorage();
+  let selectedTasks = [];
+
+  if (searchInput.value === '') {
+    renderStorage();
+    return;
+  }
+
+
+  tasks.forEach((item) => {
+    let itemPartialText = item.text.split('').slice(0, searchInput.value.length).join('');
+    if (itemPartialText === searchInput.value) {
+      selectedTasks.push(item);
+    }
+  });
+
+  renderStorage(selectedTasks);
+};
 
 renderStorage();
